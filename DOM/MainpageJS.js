20ede57c2e132ab8c9b49acd1d0b51abea206df2
PhoebuSol <133759922+PhoebuSol@users.noreply.github.com>
2023-06-16 18:55:38 +0600

    music button

--- DOM/MainpageJS.js
+++ DOM/MainpageJS.js
@@ -16,3 +16,32 @@ function scroll()
 
   window.scrollTo(0, 1350);
 }
+
+
+$(document).ready(function() {
+  $("#main-button").click(function() {
+    $(".button-options").toggleClass("show");
+  });
+
+
+  $("#play-button").click(function() {
+    var audioPlayer = $("#audio-player")[0]; // Get the audio element
+    if (audioPlayer.paused) {
+      audioPlayer.play(); // Play the audio
+    } else {
+    }
+      audioPlayer.pause(); // Pause the audio
+  });
+
+
+
+  $("#stop-button").click(function() {
+    // Add functionality for Stop button
+    console.log("Stop button clicked");
+  });
+
+  $("#repeat-button").click(function() {
+    // Add functionality for Repeat button
+    console.log("Repeat button clicked");
+  });
+});
